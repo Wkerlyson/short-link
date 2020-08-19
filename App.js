b89f2a1523be75ad5823bd2f95fcb7f465a73f85
Wkerlyson Batista <wkerlyson.batista@uece.br>
2020-08-19 12:27:42 -0300

    Implementation of the interface and consumption of the api

--- App.js
+++ App.js
@@ -1,114 +1,117 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+  SafeAreaView, StyleSheet, Text, TextInput,
+  TouchableOpacity, TouchableWithoutFeedback, Keyboard
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Clipboard from "@react-native-community/clipboard";
 
-const App: () => React$Node = () => {
+
+
+export default function App() {
+
+  const [url, setUrl] = useState('');
+  const [name, setName] = useState('');
+  const [urlFinal, setUrlFinal] = useState('');
+
+  const short = async () => {
+    Keyboard.dismiss();
+    if (url.includes('http://') || url.includes('https://')) {
+
+      await fetch(`https://cutt.ly/api/api.php?key=297b058e4549b4f9c5811c51a39d18c2ab712&short=${url}&name=${name}`)
+        .then(async response => {
+          const data = await response.json();
+
+          if (data.url.status === 3) {
+            alert('Esse nome já está em uso');
+            return;
+          }
+          if (data.url.status === 2) {
+            alert('url é inválida')
+            return;
+          }
+
+          setUrlFinal(data.url.shortLink);
+        });
+      return;
+    }
+
+    alert('Url inválida');
+  }
+
+  function copyUrl() {
+    Clipboard.setString(urlFinal);
+    alert('Url copiada com sucesso!');
+  }
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>url
+        <Text style={{ color: '#1076F7' }}>Sujeito</Text>
+        </Text>
+        <TextInput
+          style={styles.urlInput}
+          onChangeText={(text) => setUrl(text)}
+          value={url}
+          placeholder='Digite a url'
+        />
+        <TextInput
+          style={styles.urlInput}
+          onChangeText={(text) => setName(text)}
+          value={name}
+          placeholder='Nome personalizado'
+        />
+        <TouchableOpacity onPress={() => short()} style={styles.shortBtn}>
+          <Text style={{ color: '#fff' }}>ENCURTAR</Text>
+        </TouchableOpacity>
+
+        <TouchableWithoutFeedback onPress={urlFinal ? copyUrl : () => { }}>
+          <Text style={styles.finalUrl}>{urlFinal}</Text>
+        </TouchableWithoutFeedback>
+
       </SafeAreaView>
-    </>
+    </TouchableWithoutFeedback>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  title: {
+    color: '#21243d',
+    fontWeight: 'bold',
+    fontSize: 40,
+    marginBottom: 20
   },
-  body: {
-    backgroundColor: Colors.white,
+  urlInput: {
+    height: 50,
+    width: '80%',
+    borderColor: '#eeeeee',
+    color: '#616161',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    backgroundColor: '#eeeeee',
+    marginBottom: 20,
+    fontSize: 14,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  shortBtn: {
+    backgroundColor: '#ff7c7c',
+    borderRadius: 10,
+    height: 40,
+    width: '80%',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+  finalUrl: {
+    height: 40,
+    width: '80%',
+    marginTop: 20,
+    fontSize: 15,
+    textAlign: 'center'
+  }
+})
